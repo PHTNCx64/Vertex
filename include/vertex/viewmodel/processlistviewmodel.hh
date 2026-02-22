ba696aa297e41649db6e4e3115700232c261a9c9
PHTNCx64 <bleard.pulaj@protonmail.com>
2026-02-22 14:05:32 +0100

    refactor: enhance ProcessListViewModel with dispatcher integration and additional tree management methods

--- include/vertex/viewmodel/processlistviewmodel.hh
+++ include/vertex/viewmodel/processlistviewmodel.hh
@@ -7,11 +7,12 @@
 #include <memory>
 #include <string>
 #include <string_view>
+#include <functional>
 
 #include <vertex/utility.hh>
 #include <vertex/event/eventbus.hh>
 #include <vertex/model/processlistmodel.hh>
-#include <vertex/thread/vertexspscthread.hh>
+#include <vertex/thread/ithreaddispatcher.hh>
 
 namespace Vertex::ViewModel
 {
@@ -21,12 +22,11 @@ namespace Vertex::ViewModel
         ProcessListViewModel(
             std::unique_ptr<Model::ProcessListModel> model,
             Event::EventBus& eventBus,
+            Thread::IThreadDispatcher& dispatcher,
             std::string name = ViewModelName::PROCESSLIST);
 
-        [[nodiscard]] int get_processes_count() const;
-        [[nodiscard]] std::string get_process_item(long item, long column) const;
-        void set_event_callback(const std::function<void(Event::EventId, const Event::VertexEvent&)>& eventCallback);
-        void update_process_list();
+        void set_event_callback(std::move_only_function<void(Event::EventId, const Event::VertexEvent&) const> eventCallback);
+        void update_process_list() const;
         void set_filter_type(Enums::FilterType filterType) const;
         void set_sort_order() const;
         void set_filter_text(std::string_view text) const;
@@ -34,20 +34,31 @@ namespace Vertex::ViewModel
         void set_clicked_column(long column) const;
         void filter_list() const;
         void sort_list() const;
-        void set_selected_process(long itemIndex) const;
+        void set_selected_process_from_node(std::size_t nodeIndex) const;
         void clear_selected_process() const;
         void open_process() const;
+
+        [[nodiscard]] std::size_t get_root_count() const;
+        [[nodiscard]] std::size_t get_child_count(std::size_t nodeIndex) const;
+        [[nodiscard]] std::size_t get_root_node_index(std::size_t pos) const;
+        [[nodiscard]] std::size_t get_child_node_index(std::size_t parentNodeIndex, std::size_t pos) const;
+        [[nodiscard]] std::string get_node_column_value(std::size_t nodeIndex, long col) const;
+        [[nodiscard]] std::size_t get_parent_node_index(std::size_t nodeIndex) const;
+        [[nodiscard]] bool node_has_parent(std::size_t nodeIndex) const;
+        [[nodiscard]] bool node_is_visible(std::size_t nodeIndex) const;
+        [[nodiscard]] bool consume_tree_dirty() const noexcept;
 
         [[nodiscard]] int get_filter_type_index() const;
         void set_filter_type_with_persist(Enums::FilterType filterType) const;
 
+        static constexpr std::size_t INVALID_NODE_INDEX = Model::ProcessListModel::INVALID_NODE_INDEX;
+
     private:
         std::string m_viewModelName {};
         std::unique_ptr<Model::ProcessListModel> m_model {};
-        std::function<void(Event::EventId, const Event::VertexEvent&)> m_eventCallback {};
+        std::move_only_function<void(Event::EventId, const Event::VertexEvent&) const> m_eventCallback {};
 
         Event::EventBus& m_eventBus;
-
-        Thread::VertexSPSCThread m_processListThread {};
+        Thread::IThreadDispatcher& m_dispatcher;
     };
 }
--- src/vertex/viewmodel/processlistviewmodel.cc
+++ src/vertex/viewmodel/processlistviewmodel.cc
@@ -8,13 +8,15 @@
 
 #include <vertex/event/types/processopenevent.hh>
 #include <vertex/event/types/viewevent.hh>
+#include <vertex/thread/threadchannel.hh>
 
 namespace Vertex::ViewModel
 {
-    ProcessListViewModel::ProcessListViewModel(std::unique_ptr<Model::ProcessListModel> model, Event::EventBus& eventBus, std::string name)
-        : m_viewModelName(std::move(name)),
-          m_model(std::move(model)),
-          m_eventBus(eventBus)
+    ProcessListViewModel::ProcessListViewModel(std::unique_ptr<Model::ProcessListModel> model, Event::EventBus& eventBus, Thread::IThreadDispatcher& dispatcher, std::string name)
+        : m_viewModelName{std::move(name)},
+          m_model{std::move(model)},
+          m_eventBus{eventBus},
+          m_dispatcher{dispatcher}
     {
         m_eventBus.subscribe<Event::ViewEvent>(m_viewModelName, Event::VIEW_EVENT,
                                                [this](const Event::ViewEvent& event)
@@ -23,15 +25,11 @@ namespace Vertex::ViewModel
                                                });
     }
 
-    void ProcessListViewModel::set_event_callback(const std::function<void(Event::EventId, const Event::VertexEvent&)>& eventCallback) { m_eventCallback = eventCallback; }
+    void ProcessListViewModel::set_event_callback(std::move_only_function<void(Event::EventId, const Event::VertexEvent&) const> eventCallback) { m_eventCallback = std::move(eventCallback); }
 
-    std::string ProcessListViewModel::get_process_item(const long item, const long column) const { return m_model->get_process_item(item, column); }
-
-    int ProcessListViewModel::get_processes_count() const { return static_cast<int>(m_model->get_processes_count()); }
-
-    void ProcessListViewModel::update_process_list()
+    void ProcessListViewModel::update_process_list() const
     {
-        if (m_processListThread.is_busy() == StatusCode::STATUS_ERROR_THREAD_IS_BUSY) [[unlikely]]
+        if (m_dispatcher.is_channel_busy(Thread::ThreadChannel::ProcessList)) [[unlikely]]
         {
             return;
         }
@@ -40,13 +38,14 @@ namespace Vertex::ViewModel
           [this]() -> StatusCode
           {
               const auto processesStatus = m_model->get_process_list();
+              m_model->build_tree();
               m_model->filter_list();
               m_model->sort_list();
 
               return processesStatus;
           });
 
-        m_processListThread.enqueue_task(std::move(task));
+        std::ignore = m_dispatcher.dispatch_fire_and_forget(Thread::ThreadChannel::ProcessList, std::move(task));
     }
 
     void ProcessListViewModel::set_filter_type(const Enums::FilterType filterType) const
@@ -80,9 +79,9 @@ namespace Vertex::ViewModel
         m_model->sort_list();
     }
 
-    void ProcessListViewModel::set_selected_process(const long itemIndex) const
+    void ProcessListViewModel::set_selected_process_from_node(const std::size_t nodeIndex) const
     {
-        m_model->make_selected_process_from_id(itemIndex);
+        std::ignore = m_model->make_selected_process_from_node(nodeIndex);
     }
 
     void ProcessListViewModel::clear_selected_process() const
@@ -92,21 +91,79 @@ namespace Vertex::ViewModel
 
     void ProcessListViewModel::open_process() const
     {
-        if (m_model->open_process() != StatusCode::STATUS_OK) [[unlikely]]
-        {
-            return;
-        }
-
         const Class::SelectedProcess process = m_model->get_selected_process();
-        const Event::ProcessOpenEvent processOpenEvent { Event::PROCESS_OPEN_EVENT, process.get_selected_process_id().value(), process.get_selected_process_name().value() };
 
-        m_eventBus.broadcast_to(ViewModelName::PROCESSLIST, Event::ViewEvent(Event::VIEW_EVENT));
-        m_eventBus.broadcast(processOpenEvent);
+        std::packaged_task<StatusCode()> task(
+            [this, process]() -> StatusCode
+            {
+                const auto status = m_model->open_process();
+                if (status != StatusCode::STATUS_OK) [[unlikely]]
+                {
+                    return status;
+                }
+
+                const Event::ProcessOpenEvent processOpenEvent{
+                    Event::PROCESS_OPEN_EVENT,
+                    process.get_selected_process_id().value(),
+                    process.get_selected_process_name().value()};
+
+                m_eventBus.broadcast_to(ViewModelName::PROCESSLIST, Event::ViewEvent(Event::VIEW_EVENT));
+                m_eventBus.broadcast(processOpenEvent);
+
+                return StatusCode::STATUS_OK;
+            });
+
+        std::ignore = m_dispatcher.dispatch_fire_and_forget(Thread::ThreadChannel::ProcessList, std::move(task));
     }
 
     void ProcessListViewModel::set_filter_text(const std::string_view text) const
     {
         m_model->set_filter_text(std::string{text});
+    }
+
+    std::size_t ProcessListViewModel::get_root_count() const
+    {
+        return m_model->get_root_count();
+    }
+
+    std::size_t ProcessListViewModel::get_child_count(const std::size_t nodeIndex) const
+    {
+        return m_model->get_child_count(nodeIndex);
+    }
+
+    std::size_t ProcessListViewModel::get_root_node_index(const std::size_t pos) const
+    {
+        return m_model->get_root_node_index(pos);
+    }
+
+    std::size_t ProcessListViewModel::get_child_node_index(const std::size_t parentNodeIndex, const std::size_t pos) const
+    {
+        return m_model->get_child_node_index(parentNodeIndex, pos);
+    }
+
+    std::string ProcessListViewModel::get_node_column_value(const std::size_t nodeIndex, const long col) const
+    {
+        return m_model->get_node_column_value(nodeIndex, col);
+    }
+
+    std::size_t ProcessListViewModel::get_parent_node_index(const std::size_t nodeIndex) const
+    {
+        return m_model->get_parent_node_index(nodeIndex);
+    }
+
+    bool ProcessListViewModel::node_has_parent(const std::size_t nodeIndex) const
+    {
+        return m_model->node_has_parent(nodeIndex);
+    }
+
+    bool ProcessListViewModel::node_is_visible(const std::size_t nodeIndex) const
+    {
+        return m_model->node_is_visible(nodeIndex);
+    }
+
+    bool ProcessListViewModel::consume_tree_dirty() const noexcept
+    {
+        return m_model->consume_tree_dirty();
     }
 
     int ProcessListViewModel::get_filter_type_index() const
